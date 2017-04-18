e8ece29d0b04cf14ae53df3031c59eddfcf29b89
lor104 <lauren.rosentzveig@gmail.com>
2017-04-18 10:12:35 -0400

    start ex3

---
+++ ex3.rb
@@ -0,0 +1,17 @@
+# Gigasecond Birthday
+#
+# Write a function that accepts a birthdate and outputs a date that is one gigasecond in in the future (one billion seconds).
+#
+
+def future(year, month, day)
+  years = (10 ** 9) / 60 / 60 / 24 / 365
+  days = (10 ** 9) / 60 / 60 / 24 % 365
+
+  year = year + years
+  
+
+end
+
+
+(10 ** 9) / 60 / 60 / 24 / 365 # seconds - minutes - hours - days - years
+31 years and 259 days
